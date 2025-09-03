6f2beb5e39a4be417e5d0265a8133c82a5ef09de
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-03 12:08:51 -0400

    Added code to set up and initialize Passport.

--- secure-web-portal/server.js
+++ secure-web-portal/server.js
@@ -2,15 +2,30 @@ const express = require("express")
 
 const connect = require("./config/connection")
 
+const session = require("express-session")
+require("./config/passport")
+const passport = require("passport")
+
 const userRoutes = require("./routes/userRoutes")
 const bookmarkRoutes = require("./routes/bookmarkRoutes")
 
-const { PORT } = require("./utils")
+const { PORT, GITHUB_CLIENT_SECRET } = require("./utils")
 
 const run = async () => {
   await connect()
 
   const app = express()
+
+  app.use(
+    session({
+      secret: GITHUB_CLIENT_SECRET,
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+
+  app.use(passport.session())
+  app.use(passport.initialize())
 
   app.use(express.json())
 
