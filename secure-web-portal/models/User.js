ef950e31e8a1533ebcdc2db5b0a7352128c82f4f
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-03 09:45:22 -0400

    Implemented User model.

---
+++ secure-web-portal/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose")
+const { Schema, model } = mongoose
+
+const bcrypt = require("bcrypt")
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+      unique: true,
+      trim: true,
+    },
+    githubId: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+    },
+  },
+  {
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.password
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  }
+
+  next()
+})
+
+userSchema.methods.checkPassword = async function (password) {
+  return bcrypt.compare(password, this.password)
+}
+
+mongoose.set("runValidators", true)
+
+const User = model("User", userSchema)
+
+module.exports = User
