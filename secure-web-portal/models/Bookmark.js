d25dfbce6da2e419a894a7a6b2ef15a89a19e6d7
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-03 09:48:16 -0400

    Implemented Bookmark model.

---
+++ secure-web-portal/models/Bookmark.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const { Schema, model } = mongoose
+
+const bookmarkSchema = new Schema({
+  url: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+})
+
+mongoose.set("runValidators", true)
+
+const Bookmark = model("Bookmark", bookmarkSchema)
+
+module.exports = Bookmark
