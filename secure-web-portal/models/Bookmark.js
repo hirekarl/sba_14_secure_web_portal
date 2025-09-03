f5aa872db8b39244eef75fd62219cfa5c74a0e42
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-03 11:22:45 -0400

    Added toJSON method to Bookmark.

--- secure-web-portal/models/Bookmark.js
+++ secure-web-portal/models/Bookmark.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose")
 const { Schema, model } = mongoose
 
-const bookmarkSchema = new Schema({
-  url: {
-    type: String,
-    required: true,
-    trim: true,
+const bookmarkSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)
 
 mongoose.set("runValidators", true)
 
